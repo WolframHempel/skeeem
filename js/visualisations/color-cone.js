a5ae5ab2c59da4d4c850445d151c0599dde9a4c9
Wolfram Hempel <hempel.wolfram@gmail.com>
2015-11-28 17:56:14 +0100

    started working on HSL geometry

--- js/visualisations/color-cone.js
+++ js/visualisations/color-cone.js
@@ -5,6 +5,10 @@ define(function( require ){
 	var Stage = require( '../objects/stage' );
 	var tinycolor = require( 'tinycolor' );
 	var PI = Math.PI;
+	var SIN_15 = Math.sin( PI * 1.5 );
+	var COS_15 = Math.cos( PI * 1.5 );
+	var SIN_2 = Math.sin( PI * 2 );
+	var COS_2 = Math.cos( PI * 2 );
 	var ColorIndicatorCollection = require( './color-indicator-collection' );
 
 	function ColorCone( settings ) {
@@ -29,7 +33,7 @@ define(function( require ){
 	}
 
 	ColorCone.prototype.getPositionForColor = function( colorObj ) {
-		var color, alpha, hue;
+		var color, alpha, hue, f, r, x, y, z;
 
 		
 		if( this._isHsv ) {
@@ -46,19 +50,17 @@ define(function( require ){
 			alpha = hue - this._hueOffset;
 		}
 
-		var f = this._isHsv ? color.v : color.l;
-		var x = Math.sin( alpha ) * r;
-		var r = this._getRadius( f ) * color.s;
-		var y = ( this._settings.coneHeight * f ) - ( this._settings.coneHeight / 2 );
-		var z = Math.cos( alpha ) * r;
+		f = this._isHsv ? color.v : color.l;
+		r = this._getRadius( f ) * color.s;
+		x = Math.sin( alpha ) * r;
+		y = ( this._settings.coneHeight * f ) - ( this._settings.coneHeight / 2 );
+		z = Math.cos( alpha ) * r;
 
 		return this._stage.toScreenCoords( new THREE.Vector3( x, y, z ) );
 	};
 
 	ColorCone.prototype.setColor = function( colorMap ) {
 		var i, c, f, y, r, a, hue, indicatorPosition, hueOffset = ( colorMap.h / 360 ) * PI * 2;
-
-		this._hueOffset = hueOffset;
 
 		for( i = 0; i < this._relativeCoords.length; i++ ) {
 
@@ -82,9 +84,12 @@ define(function( require ){
 			// Set the color of this point
 			this._geometry.colors[ i ] = this._getColor( c[ 0 ], c[ 1 ], hue );
 		}
+
 		this._geometry.verticesNeedUpdate = true;
 		this._geometry.colorsNeedUpdate = true;
+	
 		
+
 		f = colorMap.v || colorMap.l;
 		y = ( this._settings.coneHeight * f ) - ( this._settings.coneHeight / 2 );
 		r = this._getRadius( f );
@@ -96,14 +101,32 @@ define(function( require ){
 			this._circleLine.vertices[ i ].z = r * Math.cos( a );
 		}
 
+
+		if( !this._isHsv && y > 0 ) {
+			
+
+			this._vertices.l.a.x = r * SIN_15;
+			this._vertices.l.a.y = y;
+			this._vertices.l.a.z = r * COS_15;
+
+			this._vertices.l.b.x = r * SIN_2;
+			this._vertices.l.b.y = y;
+			this._vertices.l.b.z = r * COS_2;
+
+			this._vertices.l.c.y = y;
+			
+			this._hslTopLineGeometry.verticesNeedUpdate = true;
+			y = 0;
+			r = this._settings.radius;
+		}
+
 		this._vertices.c.a.y = y;
 		this._vertices.c.a.z = r;
-
 		this._vertices.c.b.x = r * -1;
 		this._vertices.c.b.y = y;
-
 		this._vertices.c.c.y = y;
-
+		
+		this._hueOffset = hueOffset;
 		this._circleLine.verticesNeedUpdate = true;
 		this._connectingLineGeometry.verticesNeedUpdate = true;
 	};
@@ -111,7 +134,9 @@ define(function( require ){
 	ColorCone.prototype._addLines = function() {
 		this._circleLine =  new THREE.Geometry();
 		this._connectingLineGeometry = new THREE.Geometry();
+		this._hslTopLineGeometry = new THREE.Geometry();
 		var radiusGeometry = new THREE.Geometry();
+		var r = this._settings.radius;
 		
 		var y1 = this._settings.coneHeight * -0.5;
 		var y2 = this._settings.coneHeight * 0.5;
@@ -126,7 +151,7 @@ define(function( require ){
 		var yBase = this._isHsv ? y2: 0;
 		var a, v;
 		
-		material.opacity = 0.3;
+		material.opacity = 1;//0.3;
 		material.transparent = true;
 
 		for( a = this._settings.gapEnd; a < Math.PI * 1.5; a += 0.1 ) {
@@ -139,12 +164,29 @@ define(function( require ){
 		}
 
 		v = {
+			// hsl
+			l: {
+				// Triangle on top
+				a: new THREE.Vector3( 0, 0, 0 ),
+				b: new THREE.Vector3( 0, 0, 0 ),
+				c: new THREE.Vector3( 0, 0, 0 ),
+
+				// radius points
+				d: new THREE.Vector3( r * SIN_15, 0, r * COS_15 ),
+				e: new THREE.Vector3( r * SIN_2, 0, r * COS_2 )
+			},
+
+			// top
 			t: {
 				a: new THREE.Vector3( xA, yBase, yA ),
 				b: new THREE.Vector3( xB, yBase, yB ),
 				c: new THREE.Vector3( 0, y2, 0 )
 			},
+
+			// bottom
 			b: new THREE.Vector3( 0, y1, 0 ), // bottom point
+
+			// center
 			c: {
 				a: new THREE.Vector3( 0, 0, 0 ),
 				b: new THREE.Vector3( 0, 0, 0 ),
@@ -165,10 +207,23 @@ define(function( require ){
 			v.c.b, v.t.b,
 			v.b, v.t.a
 		];
+
+		this._hslTopLineGeometry.vertices = [
+			v.l.a, v.l.c,
+			//v.l.b, v.l.c,
+			v.l.a, v.l.d,
+			v.l.b, v.l.e
+		];
+
 		this._vertices = v;
 		this._stage.add( new THREE.LineSegments( this._connectingLineGeometry, material ) );
 		this._stage.add( new THREE.Line( this._circleLine, material ) );
 		this._stage.add( new THREE.Line( radiusGeometry, material ) );
+
+		if( !this._isHsv ) {
+			this._stage.add( new THREE.LineSegments( this._hslTopLineGeometry, material ) );
+		}
+		
 	};
 
 	ColorCone.prototype._createGeometry = function() {
